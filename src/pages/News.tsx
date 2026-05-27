5750686c0a4cd0087cd8d67c346db29b94a8586a
Ashot <ashot.muradyan.ait@gmail.com>
2026-05-27 19:04:48 +0400

    change files

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -44,7 +44,6 @@ const News: React.FC = () => {
         </Link>
         </div>
 
-      
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {newsItems.map((item, index) => (
             <div 
